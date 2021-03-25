158aa0c80bd5b8f5e208f65fb5acadcce91ad90e
Yang Jace Liu <ly950710@gmail.com>
2021-03-25 10:23:16 -0600

    Init commit: basic structure
    Signed-off-by: Yang Jace Liu <ly950710@gmail.com>

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+from setuptools import find_packages
+
+requirements = [
+
+]
+
+setup(name='alberta-covid-info-scraper',
+      version='0.0.1-SNAPSHOT',
+      description='A scraper to scrape COVID-19 and weather information',
+      author='Yang Liu',
+      author_email='yang.liu5@ucalgary.ca',
+      packages=find_packages(),
+      scripts=['scripts/ab-covid-scraper.py'],
+      install_requires=requirements
+      )
