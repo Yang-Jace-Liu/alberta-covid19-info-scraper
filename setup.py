645ad2ba42ffc4ffa93ff8663df1089e52d46adc
Yang Jace Liu <ly950710@gmail.com>
2021-03-25 13:58:44 -0600

    Fix #7: Fix the installation error
    Signed-off-by: Yang Jace Liu <ly950710@gmail.com>

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ requirements = [
 ]
 
 setup(name='alberta-covid-info-scraper',
-      version='0.0.1-SNAPSHOT',
+      version='0.0.1dev1',
       description='A scraper to scrape COVID-19 and weather information',
       author='Yang Liu',
       author_email='yang.liu5@ucalgary.ca',
       packages=find_packages(),
-      scripts=['scripts/ab-covid-test_scraper.py'],
+      scripts=['scripts/ab-covid-scraper.py'],
       install_requires=requirements
       )
